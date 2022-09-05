e61f9e860c99494042f4c38396358f99c2f11ca5
DaffaHardianto <112368285+DaffaHardianto@users.noreply.github.com>
2022-09-05 12:17:16 +0700

    Add files via upload

---
+++ python6-tkj3.py
@@ -0,0 +1,14 @@
+# dolar ke rupiah
+dolar = int(input("masukan nilai dolar"))
+rupiah = dolar * 14920
+print("nilai dolar ke rupiah", rupiah)
+
+# euro ke won
+euro = int(input("masukan nilai euro"))
+won = euro * 1358
+print("nilai euro ke won", won)
+
+# yen ke rupiah
+yen = int(input("masukan nilai yen"))
+rupiah = yen * 106
+print("nilai yen ke rupiah", rupiah)
