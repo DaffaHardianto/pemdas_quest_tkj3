501850697f336c61e2f5f5bb7b051a3feda6a76c
DaffaHardianto <112368285+DaffaHardianto@users.noreply.github.com>
2022-09-08 13:35:59 +0700

    Add files via upload

---
+++ Python-Latihan.py
@@ -0,0 +1,33 @@
+# Persegi
+
+sisi = int(input("masukan nilai sisi"))
+luas_p = sisi * sisi
+print("luas persegi adalah", luas_p)
+
+# Jajar Genjang
+
+alas = int(input("masukan nilai alas"))
+tinggi = int(input("masukan nilai tinggi"))
+luas_j = alas * tinggi
+print("luas jajar genjang adalah", luas_j)
+
+# segitiga
+
+alas = int(input("masukan nilai alas"))
+tinggi =int(input("masukan nilai tinggi"))
+luas_s = alas * tinggi
+print("luas segitiga adalah", luas_s)
+
+# lingkaran
+
+phi = 22/7
+r = int(input("masukan nilai r"))
+luas_l= phi * r * r
+print("luas lingkaran adalah", luas_l)
+
+# persegi panjang
+
+panjang = int(input("masukan nilai panjang"))
+lebar = int(input("masukan nilai lebar"))
+luas_pp = panjang * lebar
+print("luas persegi panjang adalah", luas_pp)
